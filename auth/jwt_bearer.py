6b4ac1774967645c87350675c694891742f7ca24
Joko Prasetyo <joko2152@gmail.com>
2024-05-19 21:43:25 +0700

    add update user data feature + adding operation status to all data response

--- auth/jwt_bearer.py
+++ auth/jwt_bearer.py
@@ -1,10 +1,8 @@
-#The goal of this file is to check whether the reques tis authorized or not [ verification of the proteced route]
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-
 from auth.jwt_handler import decode_jwt
-
+from constants.operation_status import operationStatus
 
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
@@ -16,20 +14,23 @@ class JWTBearer(HTTPBearer):
             if not credentials.scheme == "Bearer":
                 return JSONResponse(
                     {
-                    "message": "Invalid authentication scheme"
+                    "message": "Invalid authentication scheme",
+                    "operation_status": operationStatus.get("jwtGenerateError"),
                     },
                     status_code=403)
             if not self.verify_jwt(credentials.credentials):
                 return JSONResponse(
                     {
-                    "message": "Invalid token or expired token"
+                    "message": "Invalid token or expired token",
+                    "operation_status": operationStatus.get("jwtGenerateError"),
                     },
                     status_code=403)            
             return credentials.credentials
         else:
             return JSONResponse(
                     {
-                    "message": "Invalid authorization code"
+                    "message": "Invalid authorization code",
+                    "operation_status": operationStatus.get("jwtGenerateError"),
                     },
                     status_code=403)  
 
--- services/upload_file_service.py
+++ services/upload_file_service.py
@@ -7,6 +7,7 @@ from PIL import Image
 from io import BytesIO
 from config.firebase_config import firebase_config
 import uuid
+from constants.operation_status import operationStatus
 
 firebase = pyrebase.initialize_app(firebase_config())
 db = firebase.database()
@@ -15,7 +16,17 @@ storage = firebase.storage()
 async def upload_profile_picture(image_file: UploadFile = File(...), authorization: str = Depends(JWTBearer())):
 
     try:
+        if image_file is None:
+            return JSONResponse(
+                {
+                    "message": "No image is chosen (it's optional)",
+                    "operation_status": operationStatus.get("fieldValidationError")
+                },
+                status_code=422
+            )
+        
         contents = await image_file.read()
+
         
         image = Image.open(BytesIO(contents))
         
@@ -38,6 +49,7 @@ async def upload_profile_picture(image_file: UploadFile = File(...), authorizati
             {
                 "message": "Image successfully uploaded!",
                 "profile_picture_url": getProfilePictureURL,
+                "operation_status": operationStatus.get("success"),
             },
                 status_code=200
             )
@@ -51,23 +63,42 @@ async def upload_profile_picture(image_file: UploadFile = File(...), authorizati
             )
 
 async def upload_captured_image(image_file: UploadFile = File(...), folder_path: str = None, user_id: int = None, user_base_node: str = None):
-    contents = await image_file.read()
     
-    image = Image.open(BytesIO(contents))
-    
-    img_io = BytesIO()
-    image.save(img_io, format="JPEG")
-    img_io.seek(0)
+    try:
+        if image_file is None:
+                return JSONResponse(
+                    {
+                        "message": "Please provide image file to be uploaded!",
+                        "operation_status": operationStatus.get("fieldValidationError")
+                    },
+                    status_code=422
+                )
 
-    uploadImage = storage.child(folder_path + image_file.filename).put(file=img_io, content_type='image/jpeg')
+        contents = await image_file.read()
+        
+        image = Image.open(BytesIO(contents))
+        
+        img_io = BytesIO()
+        image.save(img_io, format="JPEG")
+        img_io.seek(0)
 
-    getImageURL = storage.child(folder_path + image_file.filename).get_url(uuid.uuid1())
+        uploadImage = storage.child(folder_path + image_file.filename).put(file=img_io, content_type='image/jpeg')
 
-    return JSONResponse(
-        {
-            "message": "Image successfully uploaded!",
-            "image_url": getImageURL
-        },
-            status_code=200
-        )
+        getImageURL = storage.child(folder_path + image_file.filename).get_url(uuid.uuid1())
 
+        return JSONResponse(
+            {
+                "message": "Image successfully uploaded!",
+                "image_url": getImageURL,
+                "operation_status": operationStatus.get("success")
+            },
+                status_code=200
+            )
+
+    except Exception as err:
+        return JSONResponse(
+            {
+                "message": str(err)
+            },
+                status_code=500
+            )
